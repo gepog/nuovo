c6dfa505d3a59d43a5e82496fdea1b9d9f8dde8e
gepog <gepoguo@gmail.com>
2025-06-23 14:47:14 +0200

    Fix duplicate navItems declaration

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -90,7 +90,6 @@ export const Header: React.FC<HeaderProps> = ({
     window.location.reload();
   };
 
-  const navItems = ['Home', 'My List', 'Popular', 'Practical Information'];
   const navItems = ['Home', 'My List', 'Popular'];
   return (
     <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
